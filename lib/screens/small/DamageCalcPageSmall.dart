e7d0ff2f66ee1ae8a8d6d6630a63ed74f85c14c5
raze2012 <razenkaku2012@gmail.com>
2021-01-20 21:05:45 -0800

    searchable dropdown selector working

--- lib/screens/small/DamageCalcPageSmall.dart
+++ lib/screens/small/DamageCalcPageSmall.dart
@@ -1,8 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:mgcm_tools/model/Dress.dart';
 import 'package:mgcm_tools/nav/routeConsts.dart';
 import 'package:mgcm_tools/widgets/common/AppDrawer.dart';
 import 'package:mgcm_tools/widgets/common/ComboSliderInt.dart';
 import 'package:mgcm_tools/widgets/common/ComboSliderPercent.dart';
+import 'package:searchable_dropdown/searchable_dropdown.dart';
+
+enum DisplayType
+{
+  Custom,
+  Dress
+}
+
+class HelperWrap {
+  final String _name;
+  final String _imagePath;
+  DisplayType type;
+
+  HelperWrap(this._name, this._imagePath,this.type);
+
+  Widget makeWidget() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Image.asset(
+          _imagePath,
+          width: 30,
+          height: 30,
+        ),
+        Expanded(child: Center(child: Text(_name)))
+      ],
+    );
+  }
+
+  DropdownMenuItem<HelperWrap> makeDropDown() {
+    return DropdownMenuItem(
+      child: makeWidget(),
+      value: this,
+    );
+  }
+
+  @override
+  String toString() {
+    return _name;
+  }
+}
 
 class DamageCalcPageSmall extends StatefulWidget {
   DamageCalcPageSmall({Key key, this.title}) : super(key: key);
@@ -19,8 +63,21 @@ class _DamageCalcPageSmallState extends State<DamageCalcPageSmall> {
   double _skillEnhanceBonus = 0;
   double _critBonus = 0;
 
+  HelperWrap _selectedItem;
+  List<DropdownMenuItem<HelperWrap>> _dropdownItems;
+
+  @override
+  void initState() {
+    var dresses = Hive.box<Dress>('dresses').values.toList(growable: false);
+    _dropdownItems = [HelperWrap("Custom", 'dress/placeholder.png',DisplayType.Custom).makeDropDown()];
+    _dropdownItems.addAll(dresses.map((dress) => HelperWrap(dress.name, 'assets/dress/${dress.id}.png',DisplayType.Dress).makeDropDown()));
+
+    _selectedItem = _dropdownItems[0].value;
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
         drawer: new AppDrawer(
           currentRoute: DamageCalcRoute,
@@ -38,6 +95,18 @@ class _DamageCalcPageSmallState extends State<DamageCalcPageSmall> {
                   child: ListView(
                     shrinkWrap: true,
                     children: [
+                      SearchableDropdown.single(
+                        value: _selectedItem,
+                        items: _dropdownItems,
+                        dialogBox: false,
+                        isExpanded: true,
+                        menuConstraints: BoxConstraints.tight(Size.fromHeight(350)),
+                        onChanged: (item) {
+                          setState(() {
+                            _selectedItem = item;
+                          });
+                        },
+                      ),
                       ComboSliderInt("Attack", _atk, 100, 5000, (value) {
                         setState(() {
                           _atk = value;
